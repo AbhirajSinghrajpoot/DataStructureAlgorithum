5451c912dfadc94f029b1300698152b675743a39
Abhiraj Singh Rajpoot <145330794+AbhirajSinghrajpoot@users.noreply.github.com>
2026-03-06 21:25:10 +0530

    Fix formatting of printf statement in array.c

--- array.c
+++ array.c
@@ -4,7 +4,7 @@ int main()
     int a[10],i;
     for(i=0;i<10;i++)
     {
-    printf("enter the value");
+    printf("enter the value"); 
     scanf("%d",&a[i]); 
     }
 printf("\n array element are");
